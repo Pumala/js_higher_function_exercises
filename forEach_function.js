650458ae2dbc9cdd75a255bc7d1b81c96667d2ec
Carolyn Lam <caro_lyn_@hotmail.com>
2016-10-28 11:15:00 -0400

    update forEach_function.js logic

--- forEach_function.js
+++ forEach_function.js
@@ -1,4 +1,5 @@
-// Implement a forEach function which takes two arguments: an array arr and a function fun. It will call fun passing each item in arr to fun as the first argument. Example:
+// Implement a forEach function which takes two arguments: an array arr and a function fun.
+// It will call fun passing each item in arr to fun as the first argument. Example:
 
 var arr = [
   { name: 'Bob' },
@@ -14,6 +15,57 @@ var arr = [
 // Hello, Joe!
 // You can use a loop in the implementation of this function.
 
-var greeting = arr.forEach(function(arr, fun) {
-  console.log("Hello, " + arr.name + "!");
+// var greeting = arr.forEach(function(arr, fun) {
+//   console.log("Hello, " + arr.name + "!");
+// });
+
+var names = ["Mika", "Sasha", "Paulina"];
+
+function greetPerson(name) {
+  console.log("Hello, " + name + "!");
+}
+
+var greetings = names.map(function(a) {
+  return greetPerson(a);
 });
+
+
+
+
+
+// greetPerson(names.map(function(a) {
+//   return greetPerson(a);
+// }));
+
+// console.log(greetings);
+
+
+
+// var names = ["Mika", "Sasha", "Paulina"];
+//
+// function greetPerson(name) {
+//   console.log("Hello, " + name.map(function(a) {
+//     return a;
+//   }) + " !");
+// }
+//
+// greetPerson(names);
+
+
+
+
+//
+// var greeting = names.forEach(function(names, fun) {
+//   fun(arr);
+// });
+//
+// var greetPerson = function(name) {
+//   console.log("Hello, " + name + "!");
+// };
+//
+//
+//
+//
+//
+//
+//
